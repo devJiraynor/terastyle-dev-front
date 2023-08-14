058f31d597e773739ff8d78b6aa4fec900dd7295
devJiraynor <jiraynor@naver.com>
2023-08-15 01:22:57 +0900

    2023.08.14.

---
+++ src/constants/index.ts
@@ -0,0 +1,92 @@
+export const teraTypeMenu = [
+    {
+        icon: 'normal-icon',
+        color: '#9A9792',
+        text: '노말'
+    },
+    {
+        icon: 'fire-icon',
+        color: '#FB4311',
+        text: '불'
+    },
+    {
+        icon: 'water-icon',
+        color: '#0083C2',
+        text: '물'
+    },
+    {
+        icon: 'electric-icon',
+        color: '#FACA00',
+        text: '전기'
+    },
+    {
+        icon: 'grass-icon',
+        color: '#35D32F',
+        text: '풀'
+    },
+    {
+        icon: 'ice-icon',
+        color: '#1DC1DD',
+        text: '얼음'
+    },
+    {
+        icon: 'fighting-icon',
+        color: '#FCAB00',
+        text: '격투'
+    },
+    {
+        icon: 'poison-icon',
+        color: '#A442BB',
+        text: '독'
+    },
+    {
+        icon: 'ground-icon',
+        color: '#C6883D',
+        text: '땅'
+    },
+    {
+        icon: 'flying-icon',
+        color: '#8BBDCB',
+        text: '비행'
+    },
+    {
+        icon: 'psychic-icon',
+        color: '#EF5365',
+        text: '에스퍼'
+    },
+    {
+        icon: 'bug-icon',
+        color: '#C2D501',
+        text: '벌레'
+    },
+    {
+        icon: 'rock-icon',
+        color: '#AEA981',
+        text: '바위'
+    },
+    {
+        icon: 'ghost-icon',
+        color: '#805474',
+        text: '고스트'
+    },
+    {
+        icon: 'dragon-icon',
+        color: '#6073E6',
+        text: '드래곤'
+    },
+    {
+        icon: 'dark-icon',
+        color: '#5F504D',
+        text: '악'
+    },
+    {
+        icon: 'steel-icon',
+        color: '#83A7AF',
+        text: '강철'
+    },
+    {
+        icon: 'fairy-icon',
+        color: '#F1A5D6',
+        text: '페어리'
+    },
+]
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, ChangeEvent } from 'react';
 import './style.css';
+import { teraTypeMenu } from '../../constants';
 
 export default function Home() {
 
@@ -8,6 +9,8 @@ export default function Home() {
   const [searchWord, setSearchWord] = useState<string>('');
   // description: 테라스탈 타입 버튼 클릭 상태 //
   const [dropClick, setDropClick] = useState<boolean>(false);
+  // description: 테라스탈 타입 버튼 클릭 상태 //
+  const [autoCompleteList, setAutoCompleteList] = useState<string[]>([]);
   
   //          function          //
   
@@ -24,6 +27,26 @@ export default function Home() {
   //          component          //
   
   const DropDownState = () => {
+    //          state          // 
+    //          function          //
+    //          event handler          //
+    //          component          //
+    interface MenuItemProps {
+      icon: string;
+      color: string;
+      text: string;
+    }
+
+    const MenuItem = ({ icon, color, text }: MenuItemProps) => {
+      return (
+        <div className='main-type-menu-item'>
+          <div className={icon}></div>
+          <div className='type-text-icon' style={{ backgroundColor: color }}>{text}</div>
+        </div>
+      )
+    }
+    //          effect          //
+    //          render          //
     return (
       <div className='main-search-type'>
         <div className='main-search-type-logo-default-icon background-setting'></div>
@@ -31,34 +54,7 @@ export default function Home() {
         <div className='drop-icon-up background-setting' style={{ marginRight: '16px' }} onClick={onDropClickEvent}></div>
         <div className='main-type-menu'>
           <div className='main-type-menu-container'>
-            <div className='main-type-menu-item'>
-              <div className='dragon-icon background-setting'></div>
-              <div className='type-text-icon' style={{ backgroundColor: '#6073E6' }}>드래곤</div>
-            </div>
-            <div className='main-type-menu-item'>
-              <div className='thunder-icon background-setting'></div>
-              <div className='type-text-icon' style={{ backgroundColor: '#FACA00' }}>번개</div>
-            </div>
-            <div className='main-type-menu-item'>
-              <div className='dragon-icon background-setting'></div>
-              <div className='type-text-icon'>드래곤</div>
-            </div>
-            <div className='main-type-menu-item'>
-              <div className='dragon-icon background-setting'></div>
-              <div className='type-text-icon'>드래곤</div>
-            </div>
-            <div className='main-type-menu-item'>
-              <div className='dragon-icon background-setting'></div>
-              <div className='type-text-icon'>드래곤</div>
-            </div>
-            <div className='main-type-menu-item'>
-              <div className='dragon-icon background-setting'></div>
-              <div className='type-text-icon'>드래곤</div>
-            </div>
-            <div className='main-type-menu-item'>
-              <div className='dragon-icon background-setting'></div>
-              <div className='type-text-icon'>드래곤</div>
-            </div>
+            {teraTypeMenu.map(item => <MenuItem icon={item.icon} color={item.color} text={item.text} />)}
           </div>
         </div>
       </div>
@@ -83,12 +79,17 @@ export default function Home() {
         {/* // TODO: 메인 로고 이미지 선택시 해당 포켓몬으로 검색되게 */}
         <div className='main-logo-image background-setting'></div>
         <div className='main-logo-text background-setting'></div>
-        <div className={dropClick ? 'main-search-container-down' : 'main-search-container-up'}>
+        <div className='main-search-container' style={{ borderRadius: `54px 54px ${autoCompleteList.length !== 0 ? '0px' : '54px'} ${dropClick ? '0px' : '54px'}` }}>
             {dropClick ? <DropDownState /> : <DropUpState />}
             <div className='divider'></div>
             <div className='main-search-input-box'>
                 <input className='main-search-input' placeholder='포켓몬 이름을 입력해주세요.' value={searchWord} onChange={onSearchWordChangeEvent} />
                 { searchWord !== '' && <div className='close-round-duotone-icon background-setting'></div> }
+                { autoCompleteList.length !== 0 && (
+                  <div className='search-auto-completion-box'>
+
+                  </div>
+                )}
                 <div className='search-icon background-setting' style={{ marginLeft: '16px' }}></div>
             </div>
         </div>
